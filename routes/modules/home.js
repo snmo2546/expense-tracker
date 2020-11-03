fa64436ff093c08c29b71ff001501a367b6b7952
snmo2546 <snmo2546@gmail.com>
2020-11-03 16:50:13 +0800

    feat: modify index.handlebars

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,13 @@
 // include Express and Express router
 const express = require('express')
+const Record = require('../../models/record')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
