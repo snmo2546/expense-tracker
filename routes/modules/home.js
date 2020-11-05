4c0db3a8b6ec8c6cf41928c46e064228762e0a37
snmo2546 <snmo2546@gmail.com>
2020-11-05 13:37:29 +0800

    feat: modify index page filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,25 +3,23 @@ const express = require('express')
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 const calculateTotalAmount = require('../../models/functions/calculateTotalAmount')
+const showSelectedCategory = require('../../models/functions/showSelectedCategory')
 const router = express.Router()
 
 // set routes
 router.get('/', (req, res) => {
-  const selectedFilter = req.query.filter
-
+  let selectedFilter = req.query.filter
   Category.find()
     .lean()
     .then(categories => {
       Record.find()
         .lean()
         .then(records => {
-          if (selectedFilter && selectedFilter !== "所有支出") {
+          if (typeof selectedFilter !== 'undefined' && selectedFilter !== "所有支出") {
             records = records.filter(record => record.categoryName === selectedFilter)
-            for (let i = 0; i < categories.length; i++) {
-              if (categories[i].categoryName === selectedFilter) {
-                categories.splice(i, 1)
-              }
-            }
+            showSelectedCategory(categories, selectedFilter)
+          } else if (selectedFilter === "所有支出") {
+            return res.render('index', { records, categories, totalAmount: calculateTotalAmount(records) })
           }
           res.render('index', { records, categories, selectedFilter, totalAmount: calculateTotalAmount(records) })
         })
