b257bd1445fc9ed88e9bbd26ea135ed34929a95c
snmo2546 <snmo2546@gmail.com>
2020-11-06 23:00:36 +0800

    feat: add create category function & modify categories.handlebars

--- routes/modules/categories.js
+++ routes/modules/categories.js
@@ -1,6 +1,7 @@
 // include Express and Express router
 const express = require('express')
 const Category = require('../../models/category')
+const icons = require('../../models/data/icons.json')
 const router = express.Router()
 
 // set routes
@@ -8,8 +9,19 @@ router.get('/', (req, res) => {
   Category.find()
     .lean()
     .then(categories => {
-      res.render('categories', { categories })
+      res.render('categories', { categories, icons })
     })
+    .catch(error => console.log(error))
+})
+
+router.post('/', (req, res) => {
+  const icon = req.body
+  console.log(icon)
+  return Category.create({
+    'category': icon.name,
+    'categoryIcon': icon.iconSelection,
+  })
+    .then(() => res.redirect('/categories'))
     .catch(error => console.log(error))
 })
 
